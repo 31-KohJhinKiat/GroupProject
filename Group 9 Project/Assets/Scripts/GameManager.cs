eda9ded777f370531fd19d96f78c8cd420d43112
JhinKiat <jhinkiat915@gmail.com>
2021-10-28 18:13:58 +0800

    Timer fixed.

--- Group 9 Project/Assets/Scripts/GameManager.cs
+++ Group 9 Project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    //timer
+    public float levelTime;
+    private float elapsedGameTime;
+    public Text TimerText;
+
+    //audio
+    private AudioSource audioSource;
+    public AudioClip collectSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // call the time function
+        elapsedGameTime -= Time.deltaTime;
+        SetTimeText(elapsedGameTime);
+
+        if (levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            TimerText.GetComponent<Text>().text = "Time left: " + FormatTime(levelTime);
+        }
+        else
+        {
+            levelTime = 0;
+            SceneManager.LoadScene("GameOverScene");
+            print("Times up!");
+
+        }
     }
+
+    // For time text
+    public void SetTimeText(float time)
+    {
+        TimerText.text = "Timer: " + FormatTime(time);
+    }
+
+    private string FormatTime(float time)
+    {
+        int intTime = (int)time;
+        int minutes = intTime / 60;
+        int seconds = intTime % 60;
+        float fraction = time * 1000;
+        fraction = (fraction % 1000);
+        string timerText = System.String.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, fraction);
+        return timerText;
+    }
+
 }
