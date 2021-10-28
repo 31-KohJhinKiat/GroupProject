e068e323e3b97b5d19efd60a8c9152221efe80f0
JhinKiat <jhinkiat915@gmail.com>
2021-10-28 18:37:46 +0800

    Need to shoot laser.

--- Group 9 Project/Assets/Scripts/PlayerScript.cs
+++ Group 9 Project/Assets/Scripts/PlayerScript.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //speed
     public int speed;
+
+    //shoot laser
+    public bool canShoot = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,19 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) && canShoot)
+        {
+            //shoot laser
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || transform.position.x >= 10)
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || transform.position.x <= -10)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
     }
 }
