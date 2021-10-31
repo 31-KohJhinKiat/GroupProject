25d9ac51bf0c1f95a0415bf2e8824a1b309fdb28
JhinKiat <jhinkiat915@gmail.com>
2021-10-31 12:34:50 +0800

    Credits have been made.

---
+++ Group 9 Project/Assets/Scripts/CreditScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditScript : MonoBehaviour
+{
+    public int creditSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= 800)
+        {
+            transform.position = new Vector3(Screen.width / 2, -600, 0);
+        }
+
+
+        transform.position = transform.position + new Vector3(0, creditSpeed * Time.deltaTime, 0);
+    }
+}
