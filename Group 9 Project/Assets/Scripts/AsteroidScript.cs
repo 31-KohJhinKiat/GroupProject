1c41fe8be56e86fa17577e14f0f584e2820c1aba
JhinKiat <jhinkiat915@gmail.com>
2021-11-02 18:54:02 +0800

    ok then

--- Group 9 Project/Assets/Scripts/AsteroidScript.cs
+++ Group 9 Project/Assets/Scripts/AsteroidScript.cs
@@ -30,7 +30,6 @@ public class AsteroidScript : MonoBehaviour
         }
     }
 
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("projectile"))
